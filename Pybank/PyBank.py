8ecd1e8fb8720eda51f761ec88cab0a75eb5f52c
xanendorf <xanendorf@gmail.com>
2018-08-27 17:25:56 -0500

    Imported CSV and OS, declared path and created dict with values

--- Pybank/PyBank.py
+++ Pybank/PyBank.py
@@ -1,11 +1,27 @@
 #Import CSV AND INIT
-import cvs 
-import init
-PyBankPath = 
+import csv 
+import os
+
+
+
+
+bank_csv = os.path.join(r"C:\Users\xanen\Documents\Homework\Module3_XAR\python-challenge\Resources\Budget_data.csv")
+with open(bank_csv, newline="") as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+    month_list = {'Month': [], 'Profit': []}
+    next(csv_reader, None)
+    for row in csv_reader:
+       
+        month = row[0]
+        profit = row[1]
+        month_list[month] = profit
+    
 
 #The total number of months included in the dataset
+
 #The total net amount of "Profit/Losses" over the entire period
 #The average change in "Profit/Losses" between months over the entire period
 #The greatest increase in profits (date and amount) over the entire period
 #The greatest decrease in losses (date and amount) over the entire period
 #In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+    print(month_list)
