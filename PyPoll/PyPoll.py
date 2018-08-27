2f62063663233e52c21bd98d0be67739aeaefbd1
xanendorf <xanendorf@gmail.com>
2018-08-27 16:43:17 -0500

    added readme, resources

---
+++ PyPoll/PyPoll.py
@@ -0,0 +1,6 @@
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote.
+#print the analysis to the terminal and export a text file with the results.
---
+++ Pybank/PyBank.py
@@ -0,0 +1,11 @@
+#Import CSV AND INIT
+import cvs 
+import init
+PyBankPath = 
+
+#The total number of months included in the dataset
+#The total net amount of "Profit/Losses" over the entire period
+#The average change in "Profit/Losses" between months over the entire period
+#The greatest increase in profits (date and amount) over the entire period
+#The greatest decrease in losses (date and amount) over the entire period
+#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
