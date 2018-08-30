e23a14bb109118d07895d373082581a04d9c1bcc
xanendorf <xanendorf@gmail.com>
2018-08-30 18:32:26 -0500

    Added for loop profit change

--- Pybank/PyBank.py
+++ Pybank/PyBank.py
@@ -1,6 +1,9 @@
 #Import CSV AND INIT
 import csv 
 import os
+import pandas as  pd
+import numpy as np
+import itertools as it
 
 #declare path of Data
 bank_csv = os.path.join(r"C:\Users\xanen\Documents\Homework\Module3_XAR\python-challenge\Resources\Budget_data.csv")
@@ -19,11 +22,22 @@ with open(bank_csv, newline="") as csvfile:
     
 
 #The total number of months included in the dataset
-    month_count = len(bank_dict)
+month_count = len(bank_dict)
 #The total net amount of "Profit/Losses" over the entire period
-    NetProfit = sum(list(bank_dict.values()))
+NetProfit = sum(list(bank_dict.values()))
 #The average change in "Profit/Losses" between months over the entire period
-
+# profit_change = []
+# x = 0
+# for value in bank_dict.values():
+#     if value != x:
+#         y = value
+#         profit_change.append(x-y)
+#     x =value
+# profit_change.pop(0)
+# Delta_Avg = np.mean(profit_change)
+profit_change = [bank_dict.values()]
+profit_change2 = it.accumulate(profit_change)
+Delta_Avg2 = np.mean(profit_change2)
 
  #The greatest increase in profits (date and amount) over the entire period
    
@@ -41,5 +55,7 @@ print(f"{month_count} total monthly entries")
 print(NetProfit)
 print(f"The greatest increase in profit occured in  {Greatest_increase_date} with a profit of {Greatest_increase_profit}")
 print(f"The greatest decrease in profit occured in  {Greatest_decrease_date} with a profit of {Greatest_decrease_profit}")
-
-print(deltas)
+#print(profit_change)
+#print(Delta_Avg)
+print(profit_change2)
+print(Delta_Avg2)
