23d965425344542bda56a1f5890aef7feffce83b
xanendorf <xanendorf@gmail.com>
2018-09-04 18:43:41 -0500

    Electoral counter works, formmated mostly, finishing output

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -2,68 +2,61 @@
 
 import os
 from collections import Counter
+import copy
 import csv
 
+
+candidates = []
+counties_d = {}
+counties = []
+results = {}
+Winner_Votes = 0
+
+totals_d = {"Total Votes" : 0, "Percentage Won" : 0}         
 #declare path of Data
 poll_csv = os.path.join("..\Resources\election_data.csv")
-
 # Importing Files
 with open(poll_csv, newline="") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
-#Skipping header 
     next(csv_reader, None)
-
-    counties_d = {}
-    counties = []
-    candidates = []
-    candidates_d = {}
-    
 #Making Lists from dataset
     for row in csv_reader:
        counties.append(row[1])
        candidates.append(row[2])
        counties_d.setdefault(row[1], 0)
-       candidates_d.setdefault(row[2],counties_d)
-
-      
-zipped = zip(candidates, counties)
-c = Counter(zipped)
-for i in candidates_d.keys():
-    for n in counties_d.keys():
-        x = c[i, n]
-        counties_d[n] = x
-    candidates_d[i] = counties_d
-    counties_d.clear
-    
-    # candidates_d[canidates] = {counties_d[val2], count}
-
-    
-
-
-
-        
-        
        
 
-               
-    
-
-    # Results_Dict= {
-    #     Canidate1 : { County 1 : Vote_count, County 2: Vote Count ...
-    #     Canidate2 : { County 1 : Vote_count, County 2: Vote Count ...
-    #     }
-    # }
+counties_d.update(totals_d)  
 
 
+zipped =  zip(candidates, counties)
+counted = Counter(zipped)
 
-
-    print(candidates_d)
-    print(len(counties))
 
     
+for (x, y) in sorted(counted):
+    if not x in results.keys():          
+        results[x] = copy.deepcopy(counties_d)
+    results[x][y] = counted[x, y]
+for x in results.keys():
+    results[x]["Total Votes"] = sum(results[x].values())
+    results[x]["Percentage Won"] = results[x]["Total Votes"] / len(counties) * 100
+    if results[x]['Total Votes'] > Winner_Votes:
+        Winner_Votes = results[x]["Total Votes"]
+        Winner_Percent = results[x]["Percentage Won"]
+        Winner = x
+        
+
+
 #The total number of votes cast
+Total_Votes = len(counties)
 #A complete list of candidates who received votes
-#The percentage of votes each candidate won
 #The total number of votes each candidate won
 #The winner of the election based on popular vote.
+
 #print the analysis to the terminal and export a text file with the results.
+print(f"\n----------------------------\n")
+print(f"Here is a Summary of the Election\n")
+print(f"The Winner is {Winner} with {Winner_Votes}  Votes or {'{:06.2f}'.format(Winner_Percent)}  % of the vote\n\n")
+print(f"\n----------------------------\n")
+print(f"\Electoral Analysis\n")
