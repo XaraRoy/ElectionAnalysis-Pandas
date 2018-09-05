19cff7d5a83dd6f23859931f7919b3486f7d433b
xanendorf <xanendorf@gmail.com>
2018-09-04 19:13:52 -0500

    Electoral summary prints to terminal, need to remove two "counties"

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -10,6 +10,7 @@ candidates = []
 counties_d = {}
 counties = []
 results = {}
+totals = {}
 Winner_Votes = 0
 
 totals_d = {"Total Votes" : 0, "Percentage Won" : 0}         
@@ -38,6 +39,7 @@ for (x, y) in sorted(counted):
     if not x in results.keys():          
         results[x] = copy.deepcopy(counties_d)
     results[x][y] = counted[x, y]
+print(results)
 for x in results.keys():
     results[x]["Total Votes"] = sum(results[x].values())
     results[x]["Percentage Won"] = results[x]["Total Votes"] / len(counties) * 100
@@ -45,18 +47,43 @@ for x in results.keys():
         Winner_Votes = results[x]["Total Votes"]
         Winner_Percent = results[x]["Percentage Won"]
         Winner = x
-        
+    totals[x] = results[x]["Total Votes"]
 
 
 #The total number of votes cast
-Total_Votes = len(counties)
 #A complete list of candidates who received votes
 #The total number of votes each candidate won
+
 #The winner of the election based on popular vote.
 
+
+
+
+
+
 #print the analysis to the terminal and export a text file with the results.
+print(f"\n----------------------------")
+print(f"Here is a Summary of the Election")
+print(f"----------------------------\n\n")
+print(f"The Winner is {Winner} with {Winner_Votes}  Votes or {'{:04.2f}'.format(Winner_Percent)}% of {len(counties)} total votes!\n\n")
 print(f"\n----------------------------\n")
-print(f"Here is a Summary of the Election\n")
-print(f"The Winner is {Winner} with {Winner_Votes}  Votes or {'{:06.2f}'.format(Winner_Percent)}  % of the vote\n\n")
-print(f"\n----------------------------\n")
-print(f"\Electoral Analysis\n")
+print(f"Electoral Analysis\n")
+print(f"----------------------------\n\n")
+for c in counties_d.keys():
+    if c != 'Percentage Won' or 'Total Votes':
+        County_Votes = 0
+        County_Total = 0
+  
+        print(f"Voting summary {c} county.\n\n")
+        for n in results.keys():
+            County_Total += results[n][c]
+            print(f" {n} had {results[n][c]} votes")
+            if results[n][c] > County_Votes:
+               County_Votes = results[n][c]
+               County_Percent = County_Votes / County_Total * 100
+               County_winner = n
+        
+        
+        print(f"There were {County_Total} votes in {c} county this year")
+        print(f"The winner of {c} county is {County_winner} with {County_Votes} votes or {'{:04.2f}'.format(County_Percent)}%\n")
+        print(f"----------------------------\n\n")
